65e5511efd0611ad117d3c4dd1d9d050a5f58da7
Razvan Irimie <razvan.irimie@zedra.com>
2020-09-29 16:17:48 +0300

    Merged From Master

---
+++ Provisioning/New_Office365_Team_ProvisionSite.ps1
@@ -0,0 +1,61 @@
+﻿param 
+(
+    [Parameter(Mandatory=$true)]
+    [string]$SiteUrl
+)
+
+function Download-PnPTemplate($templateName) {
+    $sas = Get-AutomationVariable –Name 'SASBlob'
+
+    $context = New-AzureStorageContext –StorageAccountName "mmsharepoint"  –StorageAccountKey $sas
+
+    Get-AzureStorageBlobContent –Context $context –Container "pnp" –Blob $templateName –Destination "C:\Users\$templateName"
+}
+
+$azureSPOcreds = Get-AutomationPSCredential –Name 'AzureAutomationSPO'
+$Password = $azureSPOcreds.GetNetworkCredential().Password
+$global:SecPassword = $azureSPOcreds.Password
+$global:ClientID = $azureSPOcreds.UserName
+
+$cert = Get-AutomationCertificate –Name 'AzureAutomationSPOAccess'
+
+$pfxCert = $cert.Export(3 ,$Password) # 3=Pfx
+$global:CertPath = Join-Path "C:\Users" "SPSiteModification.pfx"
+Set-Content –Value $pfxCert –Path $global:CertPath –Force –Encoding Byte | Write-Verbose
+    
+if (Test-Path $global:CertPath)
+{
+    $global:PnpCert = Get-PnPAzureCertificate –CertificatePassword $global:SecPassword –CertificatePath $global:CertPath
+}
+# Alternatively Legacy credentials
+# $credentials = Get-AutomationPSCredential -Name 'MM_O365'
+Set-PnPTraceLog	–On –Level Debug 
+Try
+{
+    Connect-PnPOnline –CertificatePath $global:CertPath `
+                    –CertificatePassword $global:SecPassword `
+                    –Tenant $global:tenantConfig.Settings.Azure.AADDomain `
+                    –ClientId $global:ClientID `
+                    –Url $SiteUrl
+    # Connect-PnPOnline -Credentials $credentials -Url $SiteUrl
+    
+    $templateName="Template_TeamsBasic.xml"
+    Download–PnPTemplate($templateName);
+    $templatePath="C:\Users\$templateName";
+
+    Write-Output "$(Get-Date –Format u) Template to be used is $templatePath"
+
+    Write-Output "$(Get-Date –Format u) Provisioning started"
+    Apply–PnPProvisioningTemplate –Path $templatePath 
+}
+Catch
+{
+    Write-Output (Get-Date –Format u)" "$_.Exception.Message
+    Write-Output (Get-Date –Format u)" "$_.Exception.StackTrace
+}
+Finally
+{
+    Write-Output "$(Get-Date –Format u) Provisioning done!"
+    Set-PnPTraceLog –Off 
+}
+    
---
+++ Provisioning/ZNeOffice365tProvisioning.ps1
@@ -0,0 +1,49 @@
+
+
+param
+(
+    [Parameter(Mandatory=$true)]
+    [string]$alias,
+    [Parameter(Mandatory=$true)]
+    [string]$displayName,
+    [Parameter(Mandatory=$true)]
+    [string]$teamDescription,
+    [Parameter(Mandatory=$true)]
+    [string]$teamOwner
+)
+$VerbosePreference = "Continue" 
+$creds = Get-AutomationPSCredential –Name 'MM_AzureGraph'
+$global:AppID = $creds.UserName
+$global:AppSecret = $creds.GetNetworkCredential().Password
+
+# $azureSPOcreds = Get-AutomationPSCredential -Name 'AzureAutomationSPO'
+[xml]$global:tenantConfig = Get-AutomationVariable –Name 'TenantCOnfig'
+
+# Call Team creation (sub)runbook which creates Team 
+.\New_Office365_Team_CreateTeam2.ps1 `
+		–DisplayName $displayName `
+		–Alias $alias `
+		–teamdescription $teamDescription `
+		–TeamOwner $teamOwner
+
+$team = Get-PnPUnifiedGroup –Identity $global:TeamID
+
+$counter = 0
+while (($team -eq $null -or $team.SiteUrl.Contains("Unable to provision resource")) -and $counter -lt 5)
+{
+    $counter++
+    Write-Output "$(Get-Date –Format u) Team not ready yet. Wait 1min and try again $counter/5"
+    start-sleep 60
+    $team = Get-PnPUnifiedGroup –Identity $global:TeamID
+}
+Write-Output "$(Get-Date –Format u) Site Url: $($team.SiteUrl)"
+$global:SiteUrl = $team.SiteUrl
+
+.\New_Office365_Team_ProvisionSite.ps1 –SiteUrl $global:SiteUrl
+
+# Next create a Teams tab
+# https://docs.microsoft.com/en-us/graph/teams-configuring-builtin-tabs
+
+.\New_Office365_Team_FurtherSteps.ps1 `
+		–siteUrl $global:SiteUrl `
+		–teamid $teamID
